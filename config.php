96a8e136799ff61b489ed98f8de552a672b03e9a
Alya Ahmed <alyaahmedd23@gmail.com>
2025-11-06 16:39:00 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "users_db";
+$port = 3307;
+
+$conn = new mysqli($host, $user, $password, $database, $port);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " .$conn->connect_error);
+}
+
+?>
---
+++ login_register.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+require_once 'config.php';
+
+if (isset($_POST['register'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    $role = $_POST['role'];
+
+    if ($password !== $confirm_password) {
+        $_SESSION['register_error'] = 'Passwords do not match!';
+        $_SESSION['active_form'] = 'register';
+        header("Location: index.php");
+        exit();
+    }
+
+    $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email'");
+    if ($checkEmail->num_rows > 0) {
+        $_SESSION['register_error'] = 'Email is already registered!';
+        $_SESSION['active_form'] = 'register';
+        header("Location: index.php");
+        exit();
+    }
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    $conn->query("INSERT INTO users (name, email, password, role) VALUES ('$name','$email','$hashedPassword','$role')");
+
+    header("Location: index.php");
+    exit();
+}
+
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['email'] = $user['email'];
+
+            if ($user['role'] === 'admin') {
+                header("Location: admin_page.php");
+            } else {
+                header("Location: user_page.php");
+            }
+            exit();
+        }
+    }
+
+    $_SESSION['login_error'] = 'Incorrect email or password';
+    $_SESSION['active_form'] = 'login';
+    header("Location: index.php");
+    exit();
+}
+
+
+?>
